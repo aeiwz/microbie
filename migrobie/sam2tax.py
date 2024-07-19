52abb1f251d48c297b224216f085ecf824a54102
aeiwz <77935765+aeiwz@users.noreply.github.com>
2024-07-19 17:30:19 +0700

    Update sam2tax.py

--- migrobie/sam2tax.py
+++ migrobie/sam2tax.py
@@ -1,9 +1,10 @@
 import pandas as pd
 from glob import glob
+import os
 
 def extract_sam(input_sam):
     output_txt = input_sam.replace('.sam', '.txt')
-    
+
     with open(input_sam, "r") as f:
         with open(output_txt, "w") as of:
             for line in f:
@@ -17,7 +18,7 @@ def extract_sam(input_sam):
                         of.write(line[2] + "\n")
 
 class Sam2Tax:
-    
+
     def __init__(self, file_path):
         self.file_path = file_path.replace("\\", "/").rstrip("/")
         self.process_sam_files()
@@ -27,7 +28,7 @@ class Sam2Tax:
         sam_files = glob(f"{self.file_path}/*.sam")
         for file in sam_files:
             extract_sam(file)
-    
+
     def process_txt_files(self):
         files = glob(f"{self.file_path}/*.txt")
         dfs = []
@@ -40,7 +41,7 @@ class Sam2Tax:
             df4.columns = ['taxonomy', file.split("/")[-1].replace(".txt", "")]
             df4.set_index('taxonomy', inplace=True)
             dfs.append(df4)
-            
+            os.remove(file)
         df_combined = pd.concat(dfs, axis=1, sort=True).fillna(0)
         df_combined['Total'] = df_combined.sum(axis=1).astype(int)
         df_combined['Taxonomy'] = df_combined.index
@@ -49,5 +50,5 @@ class Sam2Tax:
         df_tax.columns = ['Kingdom', 'Phylum', 'Class', 'Order', 'Family', 'Genus', 'Species']
         df_tax.fillna("Unknown", inplace=True)
         df_combined_2 = pd.concat([df_combined, df_tax], axis=1)
+
         return df_combined_2
-    
